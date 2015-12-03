6853a952a8b318d016de2bc7eb2429badd8b4f0b
Silvano Galliani <silvano.galliani@geod.baug.ethz.ch>
2015-12-03 15:30:12 +0100

    fix cmake + image io

--- main.cpp
+++ main.cpp
@@ -660,11 +660,11 @@ static int runFusibile (int argc,
         //char outputPath[256];
         //sprintf(outputPath, "%s.png", id_string);
 
-        if( access( (id_string + ".png").c_str(), R_OK ) != -1 )
+        if( access( (inputFiles.images_folder + id_string + ".png").c_str(), R_OK ) != -1 )
             inputFiles.img_filenames.push_back((id_string + ".png"));
-        else if( access( (id_string + ".jpg").c_str(), R_OK ) != -1 )
+        else if( access( (inputFiles.images_folder + id_string + ".jpg").c_str(), R_OK ) != -1 )
             inputFiles.img_filenames.push_back((id_string + ".jpg"));
-        else if( access( (id_string + ".ppm").c_str(), R_OK ) != -1 )
+        else if( access( (inputFiles.images_folder + id_string + ".ppm").c_str(), R_OK ) != -1 )
             inputFiles.img_filenames.push_back((id_string + ".ppm"));
     }
     size_t numImages = inputFiles.img_filenames.size ();
